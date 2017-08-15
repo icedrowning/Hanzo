5655a6a4fd32d52deb73131ce5a5fe191c71c030
icedrowning <575307062@qq.com>
2017-08-16 01:33:04 +0800

    新增的msg表

--- 数据库初始化.py
+++ 数据库初始化.py
@@ -3,20 +3,38 @@
 #导入你需要的库
 import sqlite3
 import os
-print('当前路径为：'+ os.getcwd())
+print('数据库当前路径为：'+ os.getcwd())
 #建立与数据库的连接，如果当前路径没有该数据库则会创建
-connection=sqlite3.connect('userinfo.db')
+connection=sqlite3.connect('main.db')
 #创建数据游标
 cursor=connection.cursor()
 #执行一些SQL操作
-sql='''create table userinfomation(
+sql='''create table userinfo(
 id  integer primary key  unique not null,
-nickname text,
+name text,
 passwd text,
+sex text,
+signature text,
 email text,
-image text)'''
+image text,
+Reserved1 text,
+Reserved2 text,
+Reserved3 text)'''
 cursor.execute(sql)
 #提交所做的修改，使修改永久保留
 connection.commit()
+
+#创建对话表
+
+sql='''create table msg(
+fromid test,
+toid test,
+content,
+fire test,
+TIME DATE )'''
+cursor.execute(sql)
+connection.commit()
+
+
 #完成时关闭链接
 connection.close()
